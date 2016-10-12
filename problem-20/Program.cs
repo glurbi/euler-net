86e60f635d53d927836e3a2978472e472efa92fd
Vincent Gay <glurbi@gmail.com>
2016-10-12 22:52:37 +0200

    problem 20

---
+++ problem-20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 100).Select(x => new BigInteger(x));
+            var fac = numbers.Aggregate((acc, x) => acc * x);
+            var sum = fac.ToString().Select(c => Char.GetNumericValue(c)).Aggregate((acc, x) => acc + x);
+            Console.WriteLine(fac);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
