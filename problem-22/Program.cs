37a6391053ce731e6c6f87486277e56a6529027c
Vincent Gay <glurbi@gmail.com>
2016-10-16 15:31:07 +0200

    problem 22

--- problem-22/Program.cs
+++ problem-22/Program.cs
@@ -10,6 +10,20 @@ namespace problem_22
     {
         static void Main(string[] args)
         {
+            var namesRaw = Properties.Resources.names;
+            var namesUnsorted = namesRaw.Split(',').Select(x => x.Substring(1, x.Length - 2));
+            var names = namesUnsorted.OrderBy(x => x);
+            var i = 1;
+            var sum = 0;
+            foreach (var name in names)
+            {
+                var value = Encoding.ASCII.GetBytes(name).Select(c => c - 64).Aggregate((acc, x) => acc + x);
+                //Console.WriteLine(name + " " + value);
+                sum += i * value;
+                i++;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
         }
     }
 }
