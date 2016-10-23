5cb979cb1986b75a5e7822e25361772a5d2e37ee
Vincent Gay <glurbi@gmail.com>
2016-10-23 14:26:34 +0200

    problem 24

---
+++ problem-24/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_24
+{
+    class Program
+    {
+        static IEnumerable<string> Permutations(string elements)
+        {
+            if (elements.Length == 1)
+                yield return elements;
+
+            foreach (var c in elements)
+            {
+                var subelements = elements.Remove(elements.IndexOf(c), 1);
+                var permutations = Permutations(subelements);
+                foreach (var permutation in permutations)
+                {
+                    yield return c + permutation;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var res = Permutations("0123456789").Skip(999999).First();
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
