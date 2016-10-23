a2494b6d05c793f4ef94f1cde2c4811ddf0ea62a
Vincent Gay <glurbi@gmail.com>
2016-10-23 21:12:44 +0200

    problem 25

---
+++ problem-25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace problem_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fn2 = new BigInteger(1);
+            var fn1 = new BigInteger(1);
+            var fn = fn2 + fn1;
+            var limit = BigInteger.Pow(new BigInteger(10), 999);
+            Console.WriteLine(limit);
+            var i = 3;
+            while (fn < limit)
+            {
+                fn2 = fn1;
+                fn1 = fn;
+                fn = fn2 + fn1;
+                i++;
+                //Console.WriteLine(fn);
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
