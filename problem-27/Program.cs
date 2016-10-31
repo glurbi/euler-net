deed8052cc8f1f3f276469a02869e06d5849a141
Vincent Gay <glurbi@gmail.com>
2016-10-31 22:10:07 +0100

    foo

---
+++ problem-27/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_27
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n == 1)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (int i = 3; i < Math.Sqrt(n); i += 2)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
