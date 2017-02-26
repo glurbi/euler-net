6ffe6fed6b10812f08cb0dbde3b1d76c382d64cf
Vincent Gay <glurbi@gmail.com>
2017-02-26 16:31:12 +0100

    pb 30

---
+++ problem-30/Program.fs
@@ -0,0 +1,18 @@
+﻿let sumOfPowers ns p =
+    ns |> List.map (fun n -> pown n p) |> List.sum
+
+let rec numberAsDigits n digits =
+    if n = 0 then
+        digits
+    else
+        n % 10 :: numberAsDigits (n / 10) digits
+
+[<EntryPoint>]
+let main argv = 
+    let candidates = [ 2 .. 1000000 ]
+    let sumOfFifthPowers = fun n -> sumOfPowers (numberAsDigits n []) 5
+    let list = candidates |> List.filter (fun n -> sumOfFifthPowers n = n)
+    let sum = list |> List.sum
+    printfn "%d" sum
+    System.Console.ReadKey() |> ignore
+    0
