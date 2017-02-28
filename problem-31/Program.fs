f006228482788ba0abc94b400d6045d134db234b
Vincent Gay <glurbi@gmail.com>
2017-02-28 23:16:41 +0100

    pb 31

--- problem-31/Program.fs
+++ problem-31/Program.fs
@@ -1,18 +1,18 @@
 ﻿[<EntryPoint>]
 let main argv = 
-    let mutable count = 0;
+    let mutable count = 0
+    let target = 200
     let currency = List.rev [ 1; 2; 5; 10; 20; 50; 100; 200 ]
-    let rec matching (coins:List<int>) (current:int) (target:int) = 
-        match (current + coins.Head) with
-        | x when x = target -> (count <- count + 1; search coins.Tail 0 target)
-        | x when x > target -> search coins.Tail x target
-        | x when x < target -> search coins x target
-        | _ -> ()
-    and search (coins:List<int>) (current:int) (target:int) =
-        if coins = []
-        then ()
-        else matching coins current target
-    search currency 0 200
+    let rec search sum current =
+        if sum = target then
+            count <- count + 1
+        elif sum > target then
+            ()
+        else
+            for c in currency do
+                if c > current then ()
+                else search (sum + c) c
+    search 0 200
     printfn "%d" count
     System.Console.ReadKey() |> ignore
     0
