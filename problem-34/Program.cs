6ff760300aeaa72cd35758bd2f3085b3520a5180
Vincent Gay <VGay@slb.com>
2018-02-10 15:22:11 +0100

    nothing

--- problem-34/Program.cs
+++ problem-34/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+// Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+// Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
 namespace problem_34
 {
     class Program
